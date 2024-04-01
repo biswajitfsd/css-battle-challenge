f7d0759246489d4c5696967b2968f5023baf5d47
Biswajit Patra <biswajit1305@gmail.com>
2024-04-01 17:09:11 +0000

    added styling and events

---
+++ events.js
@@ -0,0 +1,56 @@
+const events_data = [
+    {
+        title: 'Day One',
+        start: '2024-03-24',
+        end: '2024-03-24',
+        id: 'Day_One'
+    },
+    {
+        title: 'Day Two',
+        start: '2024-03-25',
+        end: '2024-03-25',
+        id: 'Day_Two'
+    },
+    {
+        title: 'Day Three',
+        start: '2024-03-26',
+        end: '2024-03-26',
+        id: 'Day_Three'
+    },
+    {
+        title: 'Day Four',
+        start: '2024-03-27',
+        end: '2024-03-27',
+        id: 'Day_Four'
+    },
+    {
+        title: 'Day Five',
+        start: '2024-03-28',
+        end: '2024-03-28',
+        id: 'Day_Five'
+    },
+    {
+        title: 'Day Six',
+        start: '2024-03-29',
+        end: '2024-03-29',
+        id: 'Day_Six'
+    },
+    {
+        title: 'Day Seven',
+        start: '2024-03-30',
+        end: '2024-03-30',
+        id: 'Day_Seven'
+    },
+    {
+        title: 'Day Eight',
+        start: '2024-03-31',
+        end: '2024-03-31',
+        id: 'Day_Eight'
+    },
+    {
+        title: 'Day Nine',
+        start: '2024-04-01',
+        end: '2024-04-01',
+        id: 'Day_Nine'
+    }
+];
--- index.js
+++ index.js
@@ -1,6 +1,19 @@
 const el = document.getElementById("home");
-if(!!el){
-    el.addEventListener("click", function(){
-        window.location.href ="../";
+if (!!el) {
+    el.addEventListener("click", function () {
+        window.location.href = "../";
     })
+}
+
+var date_clicked = function (selected_date) {
+    const clickedDate = new Date(selected_date);
+    const isEventPresent = events_data.some(event => {
+        const eventStartDate = new Date(event.start);
+        const eventEndDate = new Date(event.end);
+        return clickedDate >= eventStartDate && clickedDate <= eventEndDate;
+    });
+    const [year, month, date] = selected_date.split("-");
+    if (isEventPresent) {
+        window.location.href = "/css-battle-challange/" + date + month + year + "/"
+    }
 }
